6753aec6e78202e2a6a084ac916e0b18473a1ff3
Marius Kristensen <marius.kristensen@gmail.com>
2013-11-18 19:43:50 +0100

    Add support for writing OG/FG in thousands

--- js/script.js
+++ js/script.js
@@ -1,21 +1,15 @@
 function AlcoholCalc($scope) {
-    //$scope.originalGravity = parseFloat('1.040');
-    //$scope.finalGravity = parseFloat('1.009');
-//
+
     $scope.result = function() {
         var og = parseFloat($scope.originalGravity);
         var fg = parseFloat($scope.finalGravity);
 
-//        $scope.feedback = isNumeric(og);
+        if (og > 1000 && fg > 1000) {
+            fg = fg / 1000;
+            og = og / 1000;
+        }
 
-/*        if (!isNumeric(og) || !isNumeric(fg)) {
-            return 0;
-        }*/
         return (og - fg) / 0.00753;
+
     };
-
-
-    function isNumeric(n) {
-        return !isNaN(parseFloat(n)) && isFinite(n);
-    }
 }
